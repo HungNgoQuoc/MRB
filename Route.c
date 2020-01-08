54f47733cf1e1ce337acdc64ceeb85215adb20b4
HungNgoQuoc <45780098+HungNgoQuoc@users.noreply.github.com>
2020-01-08 17:18:27 +0900

    Update Route.c

--- Route.c
+++ Route.c
@@ -11,17 +11,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "graphics.h"
+#pragma comment(lib, "graphics.lib")
 
-typedef struct {
-	int xX;
-	int yY;
-} POINT;
+//typedef struct p {
+//	int x;
+//	int y;
+//} POINT;
 
-int input[9][6] = { { 1, 1, 1, 1 }, { 0, 0, 0, 0, 1 }, { 0, 1, 1, 0 }, { 0, 1,
-		0, 1, 1 }, { 0, 0, 1, 0 }, { 0, 1, 1, 1, 1 }, { 0, 0, 1, 0 }, { 0, 1, 0,
-		0, 1 }, { 0, 1, 1, 1 } };
+int gd = DETECT, gm;
+
+int input[9][6] = { { 1, 1, 1, 1 }, 
+					{ 0, 0, 0, 0, 1 }, 
+					{ 0, 1, 1, 0 },
+					{ 0, 1, 0, 1, 1 },
+					{ 0, 0, 1, 0 },
+					{ 0, 1, 1, 1, 1 },
+					{ 0, 0, 1, 0 },
+					{ 0, 1, 0, 0, 1 },
+					{ 0, 1, 1, 1 } };
+
+char output[50];
 
 int matrix[11][10];
+int matrixWall[11][10];
+
+POINT graphMatrix[11][10];
 
 void createMatrix() {
 	for (int i = 0; i < 9; i++) {
@@ -29,19 +44,26 @@ void createMatrix() {
 			if (i % 2 == 0) {
 				matrix[i + 1][j * 2 + 1] = input[i][j];
 				matrix[i + 1][j * 2] =
-						matrix[i + 1][j * 2] == 0 ?
-								input[i][j] : matrix[i + 1][j * 2];
+					matrix[i + 1][j * 2] == 0 ?
+					input[i][j] : matrix[i + 1][j * 2];
 				matrix[i + 1][j * 2 + 2] =
-						matrix[i + 1][j * 2 + 2] == 0 ?
-								input[i][j] : matrix[i + 1][j * 2 + 2];
-			} else {
+					matrix[i + 1][j * 2 + 2] == 0 ?
+					input[i][j] : matrix[i + 1][j * 2 + 2];
+			}
+			else {
 				matrix[i + 1][j * 2] = input[i][j];
 				matrix[i][j * 2] =
-						matrix[i][j * 2] == 0 ? input[i][j] : matrix[i][j * 2];
+					matrix[i][j * 2] == 0 ? input[i][j] : matrix[i][j * 2];
 				matrix[i + 2][j * 2] =
-						matrix[i + 2][j * 2] == 0 ?
-								input[i][j] : matrix[i + 2][j * 2];
+					matrix[i + 2][j * 2] == 0 ?
+					input[i][j] : matrix[i + 2][j * 2];
 			}
+		}
+	}
+
+	for (int i = 0; i < 11; i++) {
+		for (int j = 0; j < 10; j++) {
+			matrixWall[i][j] = matrix[i][j];
 		}
 	}
 }
@@ -56,46 +78,56 @@ void showMatrix() {
 		printf("\n");
 		fflush(stdout);
 	}
+
+	/*printf("\nMatrix Wall:\n");
+	for (int i = 0; i < 11; i++) {
+		for (int j = 0; j < 10; j++) {
+			printf("%3d", matrixWall[i][j]);
+			fflush(stdout);
+		}
+		printf("\n");
+		fflush(stdout);
+	}*/
 }
 
-bool isNearWall(int xX, int yY) {
-	if (xX - 1 >= 0) {
-		if (matrix[xX - 1][yY] == 1) {
+bool isNearWall(int x, int y) {
+	if (x - 1 >= 0) {
+		if (matrix[x - 1][y] == 1) {
 			return true;
 		}
 	}
-	if (xX + 1 < 11) {
-		if (matrix[xX + 1][yY] == 1) {
+	if (x + 1 < 11) {
+		if (matrix[x + 1][y] == 1) {
 			return true;
 		}
 	}
-	if (yY - 1 >= 0) {
-		if (matrix[xX][yY - 1] == 1) {
+	if (y - 1 >= 0) {
+		if (matrix[x][y - 1] == 1) {
 			return true;
 		}
 	}
-	if (yY + 1 < 10) {
-		if (matrix[xX][yY + 1] == 1) {
+	if (y + 1 < 10) {
+		if (matrix[x][y + 1] == 1) {
 			return true;
 		}
 	}
-	if (xX - 1 >= 0 && yY - 1 >= 0) {
-		if (matrix[xX - 1][yY - 1] == 1) {
+	if (x - 1 >= 0 && y - 1 >= 0) {
+		if (matrix[x - 1][y - 1] == 1) {
 			return true;
 		}
 	}
-	if (xX + 1 < 11 && yY + 1 < 10) {
-		if (matrix[xX + 1][yY + 1] == 1) {
+	if (x + 1 < 11 && y + 1 < 10) {
+		if (matrix[x + 1][y + 1] == 1) {
 			return true;
 		}
 	}
-	if (xX - 1 >= 0 && yY + 1 < 10) {
-		if (matrix[xX - 1][yY + 1] == 1) {
+	if (x - 1 >= 0 && y + 1 < 10) {
+		if (matrix[x - 1][y + 1] == 1) {
 			return true;
 		}
 	}
-	if (xX + 1 < 11 && yY - 1 >= 0) {
-		if (matrix[xX + 1][yY - 1] == 1) {
+	if (x + 1 < 11 && y - 1 >= 0) {
+		if (matrix[x + 1][y - 1] == 1) {
 			return true;
 		}
 	}
@@ -104,54 +136,41 @@ bool isNearWall(int xX, int yY) {
 
 POINT findWay(POINT p, POINT c, int value) {
 	POINT point;
-	point.xX = 0;
-	point.yY = 0;
-//	printf("65前点: (%3d,%3d)\n", p.xX, p.yY);
-//	printf("66カレント点: (%3d,%3d)\n", c.xX, c.yY);
-	if (c.xX - 1 >= 0) {
-		if (!(c.xX - 1 == p.xX && c.yY == p.yY)) {
-//			printf("1 - matrix[%d][%d]: (%3d) %d\n", c.xX - 1, c.yY,
-//					matrix[c.xX - 1][c.yY], isNearWall(c.xX - 1, c.yY));
-			if (matrix[c.xX - 1][c.yY] == value && isNearWall(c.xX - 1, c.yY)) {
-				point.xX = c.xX - 1;
-				point.yY = c.yY;
-//				printf("70 - point: (%3d,%3d)\n", point.xX, point.yY);
+	point.x = 0;
+	point.y = 0;
+
+	if (c.x - 1 >= 0) {
+		if (!(c.x - 1 == p.x && c.y == p.y)) {
+			if (matrix[c.x - 1][c.y] == value && isNearWall(c.x - 1, c.y)) {
+				point.x = c.x - 1;
+				point.y = c.y;
 			}
 		}
 	}
 
-	if (c.xX + 1 < 11) {
-		if (!(c.xX + 1 == p.xX && c.yY == p.yY)) {
-//			printf("2 - matrix[%d][%d]: (%3d)\n", c.xX + 1, c.yY,
-//					matrix[c.xX + 1][c.yY]);
-			if (matrix[c.xX + 1][c.yY] == value && isNearWall(c.xX + 1, c.yY)) {
-				point.xX = c.xX + 1;
-				point.yY = c.yY;
-//				printf("79 - point: (%3d,%3d)\n", point.xX, point.yY);
+	if (c.x + 1 < 11) {
+		if (!(c.x + 1 == p.x && c.y == p.y)) {
+			if (matrix[c.x + 1][c.y] == value && isNearWall(c.x + 1, c.y)) {
+				point.x = c.x + 1;
+				point.y = c.y;
 			}
 		}
 	}
 
-	if (c.yY - 1 >= 0) {
-		if (!(c.xX == p.xX && c.yY - 1 == p.yY)) {
-//			printf("3 - matrix[%d][%d]: (%3d)\n", c.xX, c.yY - 1,
-//					matrix[c.xX][c.yY - 1]);
-			if (matrix[c.xX][c.yY - 1] == value && isNearWall(c.xX, c.yY - 1)) {
-				point.xX = c.xX;
-				point.yY = c.yY - 1;
-//				printf("87 - point: (%3d,%3d)\n", point.xX, point.yY);
+	if (c.y - 1 >= 0) {
+		if (!(c.x == p.x && c.y - 1 == p.y)) {
+			if (matrix[c.x][c.y - 1] == value && isNearWall(c.x, c.y - 1)) {
+				point.x = c.x;
+				point.y = c.y - 1;
 			}
 		}
 	}
 
-	if (c.yY + 1 < 10) {
-		if (!(c.xX == p.xX && c.yY + 1 == p.yY)) {
-//			printf("4 - matrix[%d][%d]: (%3d)\n", c.xX, c.yY + 1,
-//					matrix[c.xX][c.yY + 1]);
-			if (matrix[c.xX][c.yY + 1] == value && isNearWall(c.xX, c.yY + 1)) {
-				point.xX = c.xX;
-				point.yY = c.yY + 1;
-//				printf("97 - point: (%3d,%3d)\n", point.xX, point.yY);
+	if (c.y + 1 < 10) {
+		if (!(c.x == p.x && c.y + 1 == p.y)) {
+			if (matrix[c.x][c.y + 1] == value && isNearWall(c.x, c.y + 1)) {
+				point.x = c.x;
+				point.y = c.y + 1;
 			}
 		}
 	}
@@ -171,72 +190,165 @@ char currentDirection(int pX, int pY, int cX, int cY) {
 		return 'U';
 }
 
-int main(void) {
+void createGraph() {
+	for (int i = 0; i < 11; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (matrix[i][j] == 0)
+			{
+				setcolor(WHITE);
+			}
+			else
+			{
+				setcolor(RED);
+			}
+
+			POINT p;
+			p.x = j * 40 + 10;
+			p.y = i * 40 + 10;
+			graphMatrix[i][j] = p;
+
+			circle(p.x, p.y, 10);
+		}
+	}
+}
+
+//POINT findWallBegin() {
+//	for (int i = 0; i < 11; i++) {
+//		for (int j = 0; j < 10; j++) {
+//			if (matrix[i][j] == 1)
+//			{
+//				POINT p;
+//				p.x = i;
+//				p.y = j;
+//				return p;
+//			}
+//		}
+//	}
+//}
+
+//void createWall() {
+//	// 前点
+//	POINT p = findWallBegin();
+//
+//	// カレント点
+//	POINT c;
+//
+//	if (matrixWall[p.x][p.y + 1] == 1) {
+//		c.x = p.x;
+//		c.y = p.y + 1;
+//	}
+//
+//	if (matrixWall[p.x + 1][p.y] == 1) {
+//		c.x = p.x + 1;
+//		c.y = p.y;
+//	}
+//	matrixWall[c.x][c.y] += 2;
+//
+//	setcolor(RED);
+//	line(graphMatrix[p.x][p.y].x, graphMatrix[p.x][p.y].y, graphMatrix[c.x][c.y].x, graphMatrix[c.x][c.y].y);
+//
+//	while (true) {
+//		if (c.x <= 0 && c.y <= 0) {
+//			break;
+//		}
+//
+//		if (c.x > 11 || c.y > 10) {
+//			break;
+//		}
+//
+//		POINT point = findWay(p, c, 1);
+//		if (point.x <= 0 && point.y <= 0) {
+//			break;
+//		}
+//
+//		if (!(c.x == point.x && c.y == point.y)) {
+//			p = c;
+//			c = point;
+//			setcolor(RED);
+//			line(graphMatrix[p.x][p.y].x, graphMatrix[p.x][p.y].y, graphMatrix[c.x][c.y].x, graphMatrix[c.x][c.y].y);
+//			matrixWall[c.x][c.y] += 2;
+//		}
+//	}
+//}
+
+int main()
+{
+	initgraph(&gd, &gm, "c:\\tc\\bgi");
+
 	bool bComeBack = false;
-	int value = 0;
+	int value = 0, index = 0;
 
 	// 前点
 	POINT p;
-	p.xX = 0;
-	p.yY = 0;
+	p.x = 0;
+	p.y = 0;
 
 	// カレント点
 	POINT c;
 
 	createMatrix();
 	showMatrix();
-	if (matrix[p.xX][p.yY + 1] == 1) {
-		c.xX = p.xX + 1;
-		c.yY = p.yY;
+	createGraph();
+	if (matrix[p.x][p.y + 1] == 1) {
+		c.x = p.x + 1;
+		c.y = p.y;
 	}
+	setcolor(13);
+	circle(graphMatrix[p.x][p.y].x, graphMatrix[p.x][p.y].y, 10);
 
-	if (matrix[p.xX + 1][p.yY] == 1) {
-		c.xX = p.xX;
-		c.yY = p.yY + 1;
+	if (matrix[p.x + 1][p.y] == 1) {
+		c.x = p.x;
+		c.y = p.y + 1;
 	}
+	setcolor(5);
+	circle(graphMatrix[c.x][c.y].x, graphMatrix[c.x][c.y].y, 10);
 
-	matrix[c.xX][c.yY] -= 1;
-
-//	printf("前点: (%3d,%3d)\n", p.xX, p.yY);
-//	printf("カレント点: (%3d,%3d)\n", c.xX, c.yY);
-
-	printf("Dang tim duong...\n");
+	matrix[c.x][c.y] -= 1;
 
 	while (true) {
-		if (c.xX <= 0 && c.yY <= 0) {
+		if (c.x <= 0 && c.y <= 0) {
 			break;
 		}
 
-		if (c.xX > 11 || c.yY > 10) {
+		if (c.x > 11 || c.y > 10) {
 			break;
 		}
 
 		POINT point = findWay(p, c, value);
-		if (point.xX <= 0 && point.yY <= 0) {
+		if (point.x <= 0 && point.y <= 0) {
 			if (!bComeBack) {
 				p = c;
 				bComeBack = true;
 				value = -1;
+				setcolor(13);
+				circle(graphMatrix[p.x][p.y].x, graphMatrix[p.x][p.y].y, 10);
+				setcolor(5);
+				circle(graphMatrix[c.x][c.y].x, graphMatrix[c.x][c.y].y, 10);
 			}
 
 			point = findWay(p, c, value);
 		}
 
-		if (!(c.xX == point.xX && c.yY == point.yY)) {
+		if (!(c.x == point.x && c.y == point.y)) {
 			p = c;
 			c = point;
-			matrix[c.xX][c.yY] -= 1;
+			setcolor(13);
+			circle(graphMatrix[p.x][p.y].x, graphMatrix[p.x][p.y].y, 10);
+			setcolor(5);
+			circle(graphMatrix[c.x][c.y].x, graphMatrix[c.x][c.y].y, 10);
+			matrix[c.x][c.y] -= 1;
 		}
 
 		//歩きの向きを判定する
-		char direction = currentDirection(p.xX, p.yY, c.xX, c.yY);
-//		printf("%c", (c.xX + c.yY) % 2 == 0 ? direction : ' ');
-		printf("%c", direction);
-//		printf("188前点: (%3d,%3d)\n", p.xX, p.yY);
-//		printf("189カレント点: (%3d,%3d)\n", c.xX, c.yY);
+		char direction = currentDirection(p.x, p.y, c.x, c.y);
+		printf("%c", (c.x + c.y) % 2 != 0 ? direction : ' ');
+		Sleep(100);
 	}
 
-	showMatrix();
+	//showMatrix();
+	
+	getch();
+	closegraph();
 
-	return EXIT_SUCCESS;
+	return 0;
 }
